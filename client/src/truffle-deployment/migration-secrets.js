3dd7f7c0e16f9cf1349d60d40741b09abffd722a
Userrick <rickwarling@gmail.com>
2019-06-11 21:00:35 +0200

    Now making succesfull transaction to the chainId: 4

--- client/src/truffle-deployment/migration-secrets.js
+++ client/src/truffle-deployment/migration-secrets.js
@@ -1,7 +1,7 @@
 const secrets = {
     mnemonic: "spare february salt census offer kangaroo clutch century hurry guide fine leg",
     ENDPOINT: "https://rinkeby.infura.io/v3/804dbc05ed56435ea836f71b0f3d9152",
-    privateKey: "8B59E6268E70817322AAC6CCFA174C99D0AE6B75236873224162EDFABE2B9406"
+    privateKey: "E97D734643AB55998F2F4B47A3EFB16C6C3166CA40DF4BA638CD530DD8A0BD38"
 }
 
 module.exports = secrets;
--- server/services/makeTransaction.js
+++ server/services/makeTransaction.js
@@ -4,69 +4,58 @@ import secrets from '../../client/src/truffle-deployment/migration-secrets';
 import HDWalletProvider from 'truffle-hdwallet-provider';
 const EthereumTx = require('ethereumjs-tx').Transaction
 
+// Every solution has functioned, thebug was always that you should have specified the chainId = 4 in every transaction!!
+// implement the following solution again: https://medium.com/finnovate-io/how-do-i-sign-transactions-with-web3-f90a853904a2
 
+
+// in this version I have done the following:
+
+// installed geth
+// https://ethereum.stackexchange.com/questions/465/how-to-import-a-plain-private-key-into-geth-or-mist
+// Paste key into notepad without any extra characters or quotations
+// Save the file as nothing_special_delete_me.txt at C:\
+// Run the command, geth account import C:\nothing_special_delete_me.txt
+// After successful import, delete the file at C:\nothing_special_delete_me.txt
+// Copeid the keytore file into this dir: $ cp -r ~/.ethereum/keystore /mnt/c/Users/rickw/Development/Blockchain-Projects/FlightSurety/server/keystore
+// Copied in the fields below
 
 const makeTransaction = async (decodedData, randomResponse) => {
   // 12 word mnemonic for HD Wallet Provider
   // You can use any provider such as the HttpProvider if you are
   // signing with private key
 
-  const provider = new Web3.providers.HttpProvider(secrets.ENDPOINT);
-  const web3 = new Web3(provider);
+  
+  // connect to any peer; using infura here
+  const web3 = new Web3(new Web3.providers.HttpProvider(secrets.ENDPOINT));
   const networkId = await web3.eth.net.getId()
-  console.log(networkId);
   const deployedAddress = FlightSuretyApp.networks[networkId].address
   const myContract = new web3.eth.Contract(FlightSuretyApp.abi, deployedAddress);
-  console.log(decodedData);
+  const addressTo = deployedAddress;
+  const txCount = await web3.eth.getTransactionCount('eb56ebc9d1bc1b8651c373cee2b38c7149ee15ab');
 
-
-// the address that will send the test transaction
-const addressFrom = '0x0be3dCDC09881e9e0773ce9287061Ff803A38d3d'
-const privKey = '8B59E6268E70817322AAC6CCFA174C99D0AE6B75236873224162EDFABE2B9406'
-
-// the destination address
-const addressTo = deployedAddress;
-
-// Signs the given transaction data and sends it. Abstracts some of the details 
-// of buffering and serializing the transaction for web3.
-function sendSigned(txData, cb) {
-  const privateKey = Buffer.from(privKey, 'hex')
-  const transaction = new EthereumTx(txData)
-  transaction.sign(privateKey)
-  const serializedTx = transaction.serialize().toString('hex')
-  web3.eth.sendSignedTransaction('0x' + serializedTx, cb)
+  // contents of keystore file, can do a fs read
+  const keystore = {"address":"eb56ebc9d1bc1b8651c373cee2b38c7149ee15ab","crypto":{"cipher":"aes-128-ctr","ciphertext":"0fcdd910b4df5fe4dbf9b8842deb4f28f9cdff39f76281359b6d88e422b013ec","cipherparams":{"iv":"2f54674d46d27d99491d7431984a38b3"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"14ad9290d92381846c556d0afd1fe945bdef710a03ac84c5897b93efb7b2f758"},"mac":"31e33b1e99a5c48a393ac9ffb46186491b14313e3502faf68d3fcae3b7680816"},"id":"a48ebf5e-bd45-4f09-990d-faa573c4681f","version":3};
+  const decryptedAccount = web3.eth.accounts.decrypt(keystore, 'karatefreak');
+  
+  const rawTransaction = {
+    "from": "eb56ebc9d1bc1b8651c373cee2b38c7149ee15ab",
+    "to": addressTo,
+    // "value": web3.utils.toHex(web3.utils.toWei("0.001", "ether")),
+    "gas": 200000,
+    "nonce": web3.utils.toHex(txCount.toString()),
+    "data": web3.utils.toHex(myContract.methods.submitOracleResponse(decodedData[0], decodedData[1], decodedData[2], decodedData[3], randomResponse).encodeABI()), 
+    "chainId": 4
+  };
+  decryptedAccount.signTransaction(rawTransaction)
+    .then(signedTx => web3.eth.sendSignedTransaction(signedTx.rawTransaction))
+    .then(receipt => console.log("Transaction receipt: ", receipt))
+    .catch(err => console.error(err));
+  // Or sign using private key from decrypted keystore file
+  /*
+  web3.eth.accounts.signTransaction(rawTransaction, decryptedAccount.privateKey)
+    .then(console.log);
+  */
 }
-
-// get the number of transactions sent so far so we can create a fresh nonce
-web3.eth.getTransactionCount(addressFrom).then(txCount => {
-  const gasLimit = 25000
-  const gasPrice = 10e9
-  const value = 123
-  // construct the transaction data
-  const txData = {
-    nonce: web3.utils.toHex(txCount.toString()),
-    gasLimit: web3.utils.toHex(gasLimit.toString()),
-    gasPrice: web3.utils.toHex(gasPrice.toString()), // 10 Gwei
-    to: addressTo,
-    from: addressFrom,
-    value: web3.utils.toHex(web3.utils.toWei(value.toString(), 'wei')),
-    data: web3.utils.toHex(myContract.methods.submitOracleResponse(decodedData[0], decodedData[1], decodedData[2], decodedData[3], randomResponse).encodeABI()) 
-  }
-  // data: web3.utils.toHex(myContract.methods.getBalance().encodeABI())
-
-  // fire away!
-  sendSigned(txData, function(err, result) {
-    if (err) return console.log('error', err)
-    console.log('sent', result)
-  })
-
-})
-
-
-
-
-}
-
 
   module.exports = {
     makeTransaction
