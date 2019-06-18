b3284953709d3407db71a8f9ea8a7a6e67c54dfb
Rick Warling <rickwarling@gmail.com>
2019-06-18 21:33:15 +0200

    Delete secrets.js

--- server/secrets.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-    mnemonic: "spare february salt census offer kangaroo clutch century hurry guide fine leg",
-    ENDPOINT: "https://rinkeby.infura.io/v3/6747de6a2a2f483cb228fc6c0cb0868d",
-    privateKey: "8B59E6268E70817322AAC6CCFA174C99D0AE6B75236873224162EDFABE2B9406"
-}
-
-module.exports = secrets;
