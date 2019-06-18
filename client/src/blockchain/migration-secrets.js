e95d0a65461ab0e7febc9936f33c1b77cd49207d
Rick Warling <rickwarling@gmail.com>
2019-06-18 21:32:56 +0200

    Delete migration-secrets.js

--- client/src/blockchain/migration-secrets.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-    address: "0x0be3dCDC09881e9e0773ce9287061Ff803A38d3d",
-    mnemonic: "spare february salt census offer kangaroo clutch century hurry guide fine leg",
-    ENDPOINT: "https://rinkeby.infura.io/v3/6747de6a2a2f483cb228fc6c0cb0868d",
-}
-
-module.exports = secrets;
